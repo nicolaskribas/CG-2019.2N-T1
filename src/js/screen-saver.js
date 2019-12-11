e40a661892fe62129dfdb74ba4cd3df40ac1ebb4
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2019-12-10 22:38:32 -0200

    Adicionando som de fundo

--- src/js/screen-saver.js
+++ src/js/screen-saver.js
@@ -28,7 +28,23 @@ scene.background = new THREE.CubeTextureLoader()
 		'ny.png',
 		'pz.png',
 		'nz.png'
-	] );
+    ] );
+    
+
+    var listener = new THREE.AudioListener();
+    camera.add( listener );
+    
+    // create a global audio source
+    var sound = new THREE.Audio( listener );
+    
+    // load a sound and set it as the Audio object's buffer
+    var audioLoader = new THREE.AudioLoader();
+    audioLoader.load( './sound/Shooting Stars.ogg', function( buffer ) {
+        sound.setBuffer( buffer );
+        sound.setLoop( true );
+        sound.setVolume( 0.5 );
+        sound.play();
+    });
 
 
 
